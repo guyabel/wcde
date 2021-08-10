0b449f9b135d9e103ff29c49e378ac6bca0fe873
Guy Abel <g.j.abel@gmail.com>
2021-08-10 10:59:23 +0800

    str_wrap_n for vectors

--- R/str_wrap_n.R
+++ R/str_wrap_n.R
@@ -15,46 +15,21 @@
 #' str_wrap_n(string = "a bb ccc dddd eeee ffffff", n = 2)
 #' str_wrap_n(string = "a bb ccc dddd eeee ffffff", n = 4)
 #' str_wrap_n(string = "a bb ccc dddd eeee ffffff", n = 8)
+#' str_wrap_n(string = c("a bb", "a bb ccc"), n = 2)
 str_wrap_n <- function(string = NULL, n = 2){
-  # words
-  w <- stringr::str_split(string = string, pattern = " ")[[1]]
-
-  if(n > length(w)){
-    n <- length(w)
-    message("Asking for more lines than words")
+  if(length(string) == 1 & length(n) == 1)
+    f <- str_wrap_n_single(string = string, n = n)
+  if(length(string) > 1){
+    s <- nn <- x <- NULL
+    f <- tibble::tibble(
+      s = string,
+      nn = n) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(x = purrr::map2(
+        .x = s, .y = nn,
+        .f = ~str_wrap_n_single(string = s, n = n))) %>%
+      dplyr::pull(x) %>%
+      unlist()
   }
-  if(n > 8){
-    stop("n is too large to look at every combination")
-  }
-
-  # breaks that minimise variance of nchar between each line
-  # if tie, chose one that miniises the variance of the number of spaces in each line
-  i <- dd <- s <- nn <- var_n <- var_s <- value <- words <- NULL
-  b <- utils::combn(x = 2:length(w)-1, m = n - 1) %>%
-    t() %>%
-    as.data.frame() %>%
-    tibble::as_tibble() %>%
-    dplyr::mutate(i = 1:dplyr::n()) %>%
-    tidyr::pivot_longer(cols = -i) %>%
-    tidyr::nest(dd = c(name, value)) %>%
-    dplyr::mutate(s = purrr::map(.x = dd, .f = ~dplyr::pull(.x, value)),
-                  nn = purrr::map(.x = s, .f = ~nchars_wrap(b = .x, w = w))) %>%
-    tidyr::unnest_wider(col = nn) %>%
-    dplyr::mutate(var_n = purrr::map_dbl(.x = n, .f = ~stats::var(x = .x)),
-                  var_s = purrr::map_dbl(.x = words, .f = ~stats::var(x = .x))) %>%
-    dplyr::filter(var_n == min(var_n)) %>%
-    dplyr::filter(var_s == min(var_s)) %>%
-    dplyr::slice(1) %>%
-    dplyr::pull(s)
-
-  f <- NULL
-  for(i in 2:length(w)-1){
-    bb <- " "
-    if(i %in% b[[1]])
-      bb <- "\n"
-    f <- paste0(f, w[i], bb)
-  }
-  f <- paste(c(f, w[length(w)]), collapse = "")
   return(f)
 }
-
